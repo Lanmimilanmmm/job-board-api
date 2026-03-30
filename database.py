f5713af469bc49ef48c895fd197af1ab539e1261
milan <lanmimilan.mm@gmail.com>
2026-03-30 02:25:53 +0200

    Add PostgreSQL with Docker healthcheck

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./jobs.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/jobboard")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
