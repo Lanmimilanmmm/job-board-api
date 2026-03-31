3e5a9e13b24d3200471b13e36a61fe22d1602cb7
milan <lanmimilan.mm@gmail.com>
2026-03-31 20:07:46 +0200

    Add Netlify to CORS origins

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ app = FastAPI(title="Job Board API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://jolly-cuchufli-4340ee.netlify.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
