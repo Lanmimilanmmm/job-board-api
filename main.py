e9d4751977d5c0e4e94b9c475de467676d9e7149
milan <lanmimilan.mm@gmail.com>
2026-03-31 19:00:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from routers import jobs, auth
 import models.user
@@ -6,6 +7,14 @@ import models.user
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Job Board API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(jobs.router)
 app.include_router(auth.router)
